d16063ab6602f26c10639edebef9e60d96120aaa
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-02 12:01:27 +0100

    refactor: created a user decorator to getUsers

--- src/users/decorator/get-user-decorator.ts
+++ src/users/decorator/get-user-decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const getUser = createParamDecorator(
+export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
     if (data) {
