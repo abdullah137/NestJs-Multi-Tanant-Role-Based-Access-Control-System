c8ca66bb509b663966212f7eba687aba0375435f
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-02 12:06:36 +0100

    feat: added jwt guard to the create role routes

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,11 +1,31 @@
-import { Post, Controller, Body } from '@nestjs/common';
+import { Post, Controller, Get, Body } from '@nestjs/common';
 import { RolesDto } from './dtos/roles.dto';
 import { RolesService } from './roles.service';
-
+import { RolesLogic } from './logic/roles.logic';
+import { GetUser } from '../users/decorator/get-user-decorator';
+import { UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../users/guard/jwt.guard';
 @Controller('roles')
 export class RolesController {
-  constructor(private rolesService: RolesService) {}
+  constructor(
+    private rolesService: RolesService,
+    private rolesLogic: RolesLogic,
+  ) {}
 
   @Post('/create')
-  createRole(@Body() body: RolesDto) {}
+  @UseGuards(JwtGuard)
+  createRole(@Body() body: RolesDto, @GetUser() user) {
+    console.log(user);
+    return this.rolesLogic.createRoles(body);
+  }
+
+  @Get('/all')
+  async getAllRoles() {
+    return this.rolesService.getRoles();
+  }
+
+  @Get('/all-user-to-roles')
+  async getAllUserToRoles() {
+    return this.rolesService.getRoleToUser();
+  }
 }
